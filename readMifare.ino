d8c8f5868db911cd26d006b2c85c96d766b46940
erutkovskis <edvards.rutkovskis@gmail.com>
2022-10-16 13:53:16 +0100

    Checkpoint 5. Reading, writing NFC. Improved code.

--- readMifare.ino
+++ readMifare.ino
@@ -67,7 +67,6 @@ Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 // variables and constants for the Serial Read handling
 const byte numChars = 32;
 char receivedChars[numChars]; // Array to store received characters from Serial Read
-uint8_t savedNumber = 0;
 boolean newData = false;
 uint8_t dataNumber = 0;
 
@@ -118,49 +117,50 @@ void loop(void) {
     {
       // We probably have a Mifare Classic card ... 
       Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
-	  
-      // Now we need to try to authenticate it for read/write access
-      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
-      Serial.println("Trying to authenticate block 4 with default KEYA value");
-      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-	  
-	  // Start with block 4 (the first block of sector 1) since sector 0
-	  // contains the manufacturer data and it's probably better just
-	  // to leave it alone unless you know what you're doing
-      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
-	  
-      if (success)
-      {
-        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
-        uint8_t data[16];
-		
-        // If you want to write something to block 4 to test with, uncomment
-		// the following line and this text should be read back in a minute
-        //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
-        // success = nfc.mifareclassic_WriteDataBlock (4, data);
-
-        // Try to read the contents of block 4
-        success = nfc.mifareclassic_ReadDataBlock(4, data);
-		
-        if (success)
-        {
-          // Data seems to have been read ... spit it out
-          Serial.println("Reading Block 4:");
-          nfc.PrintHexChar(data, 16);
-          Serial.println("");
-		  
-          // Wait a bit before reading the card again
-          delay(1000);
-        }
-        else
-        {
-          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
-        }
-      }
-      else
-      {
-        Serial.println("Ooops ... authentication failed: Try another key?");
-      }
+      Serial.println("Device not supported with this code");
+//	  
+//      // Now we need to try to authenticate it for read/write access
+//      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+//      Serial.println("Trying to authenticate block 4 with default KEYA value");
+//      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+//	  
+//	  // Start with block 4 (the first block of sector 1) since sector 0
+//	  // contains the manufacturer data and it's probably better just
+//	  // to leave it alone unless you know what you're doing
+//      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+//	  
+//      if (success)
+//      {
+//        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
+//        uint8_t data[16];
+//		
+//        // If you want to write something to block 4 to test with, uncomment
+//		// the following line and this text should be read back in a minute
+//        //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
+//        // success = nfc.mifareclassic_WriteDataBlock (4, data);
+//
+//        // Try to read the contents of block 4
+//        success = nfc.mifareclassic_ReadDataBlock(4, data);
+//		
+//        if (success)
+//        {
+//          // Data seems to have been read ... spit it out
+//          Serial.println("Reading Block 4:");
+//          nfc.PrintHexChar(data, 16);
+//          Serial.println("");
+//		  
+//          // Wait a bit before reading the card again
+//          delay(1000);
+//        }
+//        else
+//        {
+//          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+//        }
+//      }
+//      else
+//      {
+//        Serial.println("Ooops ... authentication failed: Try another key?");
+//      }
     }
     
     if (uidLength == 7)
@@ -172,6 +172,23 @@ void loop(void) {
       for (uint8_t pageNum = 4; pageNum <= 15; pageNum++){
         Serial.print("Reading page ");
         Serial.println(pageNum);
+        switch (pageNum) {
+          case 4:
+          Serial.println("Pulse Width: HB, LB; Pulse Frequency: HB, LB");
+          Serial.println("Default: 0 1 93 61");
+          break;
+        case 5:
+          Serial.println("Stimulation Time On (Duty Cycle): msec Byte, sec HB, sec LB; On/Off byte");
+          Serial.println("Default: 0 112 2 1");
+          break;
+        case 6:
+          Serial.println("Current Amplitude; Stimulation Mode; Stimulation Channel (For Single-Channel Stim)");
+          Serial.println("Default: 63 2 0 0");
+          break;
+        case 8:
+          Serial.println("Current Stimulation Channel (channel scanning mode)");
+          break;
+        }
         uint8_t data[32];
         success = nfc.mifareultralight_ReadPage (pageNum, data);
         if (success)
@@ -188,52 +205,38 @@ void loop(void) {
         }
       }
       
-      // Try to write the 32 byte data to the specified page
+      // Try to write the specified data to the specified page
       
       // Select the page to write
       uint8_t pageWr;
       Serial.println("\n\n1) Enter the page to write");
       receiveNumber();
       pageWr = saveNumber();
-      confirm();
-      Serial.print("Will write to the page: "); Serial.println(pageWr);
+      Serial.print("\nWill write to the page: "); Serial.println(pageWr);
       confirm();
 
       // Create data to write to the selected page
+
       uint8_t dataWr[4]; // 4-byte array for data
-      Serial.println("\n\nEnter byte 1"); 
-      receiveNumber();
-      dataWr[0] = saveNumber();
-      Serial.println("\n\nEnter byte 2"); 
-      receiveNumber();
-      dataWr[1] = saveNumber();
-      Serial.println("\n\nEnter byte 3"); 
-      receiveNumber();
-      dataWr[2] = saveNumber();
-      Serial.println("\n\nEnter byte 4"); 
-      receiveNumber();
-      dataWr[3] = saveNumber();
-      Serial.print("Will write the following data page: "); 
+      Serial.println("\n\n2) Enter data");
       for (byte i = 0; i < 4; i++) {
+          Serial.print("\n\nEnter byte "); Serial.println(i+1);
+          receiveNumber();
+          dataWr[i] = saveNumber();
+      }
+
+      Serial.print("\nWill write the following data page: "); 
+      for (byte i = 0; i < 4; i++) {
+          if (i == 3){
+              Serial.print(dataWr[i], HEX); Serial.print("\n");
+              continue;
+        }
         Serial.print(dataWr[i], HEX); Serial.print(", ");
       }
       confirm();
 
       nfc.mifareultralight_WritePage(pageWr, dataWr);
-      
-     
-//      for (byte n = 0; n < 4; n++) {
-//        Serial.print("Tests dataWr in HEX: "); Serial.print(dataWr[n], HEX); Serial.print(" ");
-//      }
-//      Serial.print("Test data using NFC PrintHexChar");     
-//      nfc.PrintHexChar(dataWr, 32);
-//      Serial.println("");
 
-      // Test write NFC memory with the saved values
-      // nfc.mifareultralight_WritePage(pageWr, dataWr);
-      
-      // Wait a bit before reading the card again
-      //Serial.flush();
       delay(1000);
     }
   }
@@ -268,10 +271,13 @@ uint8_t saveNumber() {
     if (newData == true) {
         dataNumber = 0;             // new for this version
         dataNumber = atoi(receivedChars);   // new for this version
-        Serial.print("\n\nThe following input has been done on the Serial Monitor: ");
+        Serial.print("\nThe following input has been received on the Serial Monitor: ");
         Serial.println(receivedChars);
-        Serial.print("\n\nData represented as Number ... ");    // new for this version
+        Serial.print("\nData represented as a Number ... ");
         Serial.println(dataNumber);     // new for this version
+        Serial.print("\nData represented as a HEX Number ... ");
+        Serial.println(dataNumber, HEX);     
+        
         newData = false;
     }
     return dataNumber;
@@ -279,9 +285,10 @@ uint8_t saveNumber() {
 
 void confirm() {
     // Wait any user input before proceeding
-  Serial.println("\n\nSend any character to proceed");
+  Serial.println("\n\nSend any character via 'Send' or press Enter once to proceed");
   while (!Serial.available());
   while (Serial.available()) {
+    delay(2);
   Serial.read();
   }
 }
